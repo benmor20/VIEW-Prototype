5f58cbd11577478ed6e75f81823b3f730c517e96
benmor20 <benmor2020@gmail.com>
2024-02-07 10:08:40 -0500

    Import HTML/CSS, setup index page

--- view/lidar/views.py
+++ view/lidar/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'lidar/index.html', {})
+
+
+def add_vehicle(request):
+    return render(request, 'lidar/add-vehicle.html', {})
+
+
+def data_upload(request):
+    return render(request, 'lidar/data-upload.html', {})
+
+
+def faq(request):
+    return render(request, 'lidar/faq.html', {})
+
+
+def vehicle_database_loading(request):
+    return render(request, 'lidar/vehicle-database-loading.html', {})
+
+
+def vehicle_database_table(request):
+    return render(request, 'lidar/vehicle-database-table.html', {})
